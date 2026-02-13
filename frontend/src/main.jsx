fec612b34bcfbe42f6ffd5cc6a48f91563b318cd
Jhonatas Alves Oliveira <jhonatard132@outlook.com>
2026-02-13 18:12:11 -0300

    PWD

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom' 
-
-// REMOVA OU COMENTE ESTA IMPORTAÇÃO
-// import { EmpresaProvider } from './contexts/EmpresaContext' 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* REMOVA O PROVIDER QUE ESTÁ ENVOLVENDO O APP */}
-      {/* <EmpresaProvider> */}
-        <App />
-      {/* </EmpresaProvider> */}
+      <App />
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+serviceWorkerRegistration.register();
---
+++ frontend/src/service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'cyrius-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request).then((response) => {
+        return response || caches.match('/index.html');
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
